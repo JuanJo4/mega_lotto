b6c982124db83bfe3c2b0ca9f737a7147b0a0ee6
JuanJo4 <juanj.gonzalezp@gmail.com>
2014-02-10 23:01:23 -0430

    Added code and tests

---
+++ lib/mega_lotto/drawing.rb
@@ -0,0 +1,17 @@
+module MegaLotto
+	class Drawing
+		NUMBERS = 5
+
+		def draw
+			NUMBERS.times.inject([]) do |arr, i|
+        		arr << single_draw
+      		end
+		end
+
+		private
+
+    	def single_draw
+      		rand(0...60)
+    	end
+	end
+end
